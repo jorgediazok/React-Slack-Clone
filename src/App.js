28fd1165dcc01dbc4f24db7f7541e3e9c142d999
Jorge <jorgediazok@gmail.com>
2020-10-28 01:43:45 -0300

    Made the header responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-      <h1>First commit</h1>
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
