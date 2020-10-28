3395cbe4bd7f17247facf5f3ebab4fc65587bd47
Jorge <jorgediazok@gmail.com>
2020-10-28 00:15:48 -0300

    all set

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCESvf0nRvWlqCpgR4FYoDiUP26WLYjM3o',
+  authDomain: 'react-slack-clone-86305.firebaseapp.com',
+  databaseURL: 'https://react-slack-clone-86305.firebaseio.com',
+  projectId: 'react-slack-clone-86305',
+  storageBucket: 'react-slack-clone-86305.appspot.com',
+  messagingSenderId: '226718263478',
+  appId: '1:226718263478:web:40430bd6cddbd2a7105e17',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
