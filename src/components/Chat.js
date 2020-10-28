60dbaea6cd11c417d7dc8cece5643554b36db39f
Jorge <jorgediazok@gmail.com>
2020-10-28 15:34:05 -0300

    fixed the bug and now channels working

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { InfoOutlined, StarBorderOutlined } from '@material-ui/icons';
+import db from '../firebase';
 import '../styles/Chat.css';
 
 function Chat() {
   const { roomId } = useParams();
+  const [roomDetails, setRoomDetails] = useState(null);
+
+  useEffect(() => {
+    if (roomId) {
+      db.collection('rooms')
+        .doc(roomId)
+        .onSnapshot((snapshot) => setRoomDetails(snapshot.data()));
+    }
+  }, [roomId]);
+
   return (
     <div className="chat">
       <div className="chat__header">
         <div className="chat__headerLeft">
           <h4 className="chat__channelName">
-            <strong># general</strong>
+            <strong># {roomDetails?.name}</strong>
             <StarBorderOutlined />
           </h4>
         </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useState, useEffect } from 'react';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from '@material-ui/icons/Create';
